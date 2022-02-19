54b391873e1859f39b2ba7a1f6b6a5dda037865a
Ammadq87 <77247515+Ammadq87@users.noreply.github.com>
2022-02-19 01:51:08 -0500

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,7 +1,12 @@
 
 
-function hi(){
-   
-   alert('hi');
-    // console.log(window.location.url);
+function saveLoginData(){
+
+    
+
 }
+
+{/* <button onclick="window.location.href='About.html' ">
+            Click
+          </button>
+          <div class="u-align-left u-form-group u-form-submit" style="width: 26.5vw; position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%);" > */}
